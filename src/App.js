fee7324200e742f21624ddef4330757f3259d68f
Dammy Knight <olaoluwaayomide10@gmail.com>
2023-09-09 04:46:06 +0100

    Adding a new friend

--- src/App.js
+++ src/App.js
@@ -22,16 +22,23 @@ const initialFriends = [
 ];
 
 export default function App() {
+	const [friends, setFriends] = useState(initialFriends);
 	const [showAddFriend, setShowAddFriend] = useState(false);
 
 	function handleShowAddFriend() {
 		setShowAddFriend((show) => !show);
 	}
+
+	function handleAddFriend(friend) {
+		setFriends((friends) => [...friends, friend]);
+		setShowAddFriend(false);
+	}
+
 	return (
 		<div className='app'>
 			<div className='sidebar'>
-				<FriendList />
-				{showAddFriend && <FormAddFriend />}
+				<FriendList friends={friends} />
+				{showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
 				<Button onClick={handleShowAddFriend}>
 					{showAddFriend ? 'Close' : 'Add friend'}
 				</Button>
@@ -50,9 +57,7 @@ function Button({ children, onClick }) {
 	);
 }
 
-function FriendList() {
-	const friends = initialFriends;
-
+function FriendList({ friends }) {
 	return (
 		<ul>
 			{friends.map((friend) => (
@@ -83,14 +88,44 @@ function Friend({ friend }) {
 	);
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
+	const [name, setName] = useState('');
+	const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		if (!name || !image) return;
+
+		const id = crypto.randomUUID();
+
+		const newFriend = {
+			id,
+			name,
+			image: `${image} ?=${id}`,
+			balance: 0,
+		};
+
+		onAddFriend(newFriend);
+
+		setName('');
+		setImage('https://i.pravatar.cc/48');
+	}
 	return (
-		<form className='form-add-friend'>
+		<form className='form-add-friend' onSubmit={handleSubmit}>
 			<label>👯‍♀️ Friend name</label>
-			<input type='text' />
+			<input
+				type='text'
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
 
 			<label>🌄 Image URL</label>
-			<input type='text' />
+			<input
+				type='text'
+				value={image}
+				onChange={(e) => setImage(e.target.value)}
+			/>
 
 			<Button>Add</Button>
 		</form>
